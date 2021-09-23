7508f8c84701c2ca53f1052170380681a43394d0
TimV22 <30288486+TimV22@users.noreply.github.com>
2021-09-23 14:26:30 +0200

    Begin week4

---
+++ Week4/assignment2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.Write("Enter target number: ");
+            int checknumber = int.Parse(Console.ReadLine());
+            int countnumberchecked = 0;
+
+
+            do
+            {
+                Console.Write("Enter a number: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number == checknumber)
+                {
+                    countnumberchecked++;
+                }
+
+            } while (number != 0);
+
+
+            Console.WriteLine($"Count of numbers equal to target number: {countnumberchecked}");
+
+            Console.ReadKey();
+        }
+    }
+}
